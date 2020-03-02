64a42c8f95e7db5fc03878587256caf021965603
Andres <afre92@gmail.com>
2020-03-01 19:25:37 -0500

    more solutions added

--- leetcode.rb
+++ leetcode.rb
@@ -89,3 +89,183 @@ end
         (1..n).bsearch { |i| is_bad_version(i) }
     end
 
+#Binary tree right side view
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {Integer[]}
+def right_side_view(root)
+    sol = []
+    if root.nil?
+        return sol
+    end
+    # make queue equals to my root
+    queue = [root]
+    while !queue.empty?
+        num_items_at_level = queue.length
+        # loop by the length of queue
+        num_items_at_level.times do |i|
+            # pull the first item from the queue and make it current
+            cur = queue.shift
+            # add left leaf from current tree to queue if there is a left leaf
+            queue.push(cur.left) if cur.left
+            # add right leaf from current tree to queue if there is a right leaf
+            queue.push(cur.right) if cur.right
+            # if there are no more item at this level
+            # how do we know where are taking the right side into the solution array ?
+            if i == num_items_at_level - 1
+                sol.push(cur.val)
+            end
+        end
+    end
+    
+    return sol
+end
+
+
+# Verifying an alien dictionary
+
+# @param {String[]} words
+# @param {String} order
+# @return {Boolean}
+def is_alien_sorted(words, order)
+    # gets indexes of each word in words array to loop
+    (0...words.length - 1).each do |i|
+        # first letter of first word
+        a = words[i][0]
+        # first letter of second word
+        b = words[i + 1][0]
+        puts b,'--'
+        j = 1
+        # if both first words are the same then keep looping through the word until find different
+        while a == b
+            a = words[i][j]
+            b = words[i + 1][j]
+            j += 1
+        end
+        #
+        return false if b.nil?
+        # return false if a comes before b 
+        # if a come before b means the words are not sorted based on the alien dictionary 
+        return false if order.index(a) > order.index(b)
+    end
+    
+    true
+end
+
+# Next greater Element I
+
+def next_greater_element(nums1, nums2)
+  stack = []
+    # create new hash where default is -1 when key is not found
+  next_greater_ele = Hash.new(-1)
+  result = []
+
+# create hash with nums2 as keys and their value is the next grater element of the same array
+  nums2.each { |n|
+    #while stask and stack.last is smaller than current item
+    while( stack.any? && stack.last < n )
+        # add last item in stack as key and the next greater item as value on the hash
+      next_greater_ele[stack.pop] = n
+    end
+    stack.push(n)
+  }
+  nums1.each { |n|
+    result << next_greater_ele[n]
+  }
+  result
+end
+
+# permutation in string
+
+
+def check_inclusion(s1, s2)
+    #create hashes with an array as key and 0 as value ?
+    map1 = Hash.new{|h,k| h[k] = 0 }
+    map2 = Hash.new{|h,k| h[k] = 0 }
+    # k is the length of the s1 string
+    k = s1.size
+    # edit hash to have each letter of string as key and number of times it appears as value
+    s1.chars.each do |ch|
+        map1[ch] += 1
+    end
+    
+    #make sliding window
+    #{"e"=>1, "i"=>1}
+    (0...k).each do |i|
+        map2[s2[i]] += 1
+    end
+    
+    return true if map1 == map2
+    
+    #update window
+    (k...s2.size).each do |i|
+        map2[s2[i]] += 1
+        map2[s2[i-k]] -= 1
+        #{"e"=>0, "i"=>1, "d"=>1}--{"i"=>0, "d"=>1, "b"=>1}--{"d"=>0, "b"=>1, "a"=>1}--
+        map2.delete(s2[i-k]) if map2[s2[i-k]] == 0
+        return true if map1 == map2
+    end
+    return false
+end
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def search(nums, target)
+    if (nums == nil || nums.length == 0)  
+        return -1 
+    end
+    
+    left = 0 
+    right = nums.length - 1
+    
+    # goal to have these bounderies meet the the index of the smallest element of the array
+    while (left< right) do 
+        midpoint = left + (right -left)/2
+        # if the elemnt on the mid point is greater than the element all the way to the right() which is how it should be on a sorted array
+        if (nums[midpoint] > nums[right]) 
+            # move left boundry to minpoint + 1 index, moving the search to the right side of array
+            left = midpoint+1
+        else
+            # move right boundy to minpoint index, moving the search to the left side of the array
+            right = midpoint
+        end
+    end
+    
+    start = left
+    left = 0 
+    right = nums.length - 1
+    
+    #if target is greater or equal our smallest number(start on sorted array)
+    # and target is smaller or greare than right limit
+    if target >= nums[start] && target <= nums[right] 
+        left = start
+    else
+        right = start
+    end
+    # regular binary search
+    while left <= right do
+        midpoint  = left + (right - left)/2
+        if nums[midpoint] == target
+            return midpoint
+        elsif nums[midpoint] < target
+            left = midpoint + 1
+        else
+            right = midpoint - 1
+        end
+        
+    
+    end
+    
+    return -1
+    
+end
